6c864305bf854186a8d9bfba16e27a47f3268910
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-05-19 21:19:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ client.on('message', message => {
     if (!client.commands.has(command)) return;
 
     try {
-        client.commands.get(command).execute(client, message, args);
+        client.commands.get(command).execute(message, args, client);
     } catch (error) {
         console.error(error);
         message.reply('コマンドの実行中にエラーが発生しました。');
