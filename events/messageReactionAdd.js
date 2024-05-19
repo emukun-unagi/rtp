7cdb82d83760426ab9a629968b5abcffcd51f7a3
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-05-19 14:51:11 +0900

    Update messageReactionAdd.js

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,21 +1,20 @@
 const config = require('../config.json');
 
 module.exports = {
-    name: 'messageReactionAdd',
-    once: true,
-    execute(client, reaction, user) {
-        if (reaction.emoji.name === '🗑️') {
-            if (reaction.message.author !== client.user) {
-                return;
-            }
-            
-            reaction.message.reactions.cache.get('🗑️').users.fetch().then(users => {
-                if (users.size === 2) {
-                    reaction.message.edit({
-                        content: '🗑️削除しました',
-                    });
-                }
-            });
-        }
-    }
+    name: 'messageReactionAdd',
+    once: true,
+    execute(client, reaction, user) {
+        if (reaction.emoji.identifier === '🗑️') {
+            if (reaction.message.author !== client.user) {
+                return;
+            }
+            reaction.message.reactions.cache.get('🗑️').users.fetch().then(users => {
+                if (users.size === 2) {
+                    reaction.message.edit({
+                        content: '🗑️削除しました',
+                    });
+                }
+            });
+        }
+    }
 };
