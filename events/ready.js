7da9d7fbc11d404fcf5516b47386f3b3a5d4ff70
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-05-19 13:28:58 +0900

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,20 +1,20 @@
 const config = require('../config.json');
 
-
 module.exports = {
-    name: 'ready',
-    once: true,
-    execute(client) {
-        const status = {
-            activities: [{
-                name: client.guilds.cache.size,
-                type: config.status.type
-            }],
-            status: config.status.presence
-        };
+    name: 'ready',
+    once: true,
+    execute(client) {
+        console.log(client.user.tag + "is online!");
 
-        client.user.setPresence(status);
-
-        console.log(`${client.user.tag} is online!`)
-    },
+        setInterval(() => {
+            const status = {
+                activities: [{
+                    name: client.guilds.cache.size + "サーバー",
+                    type: config.status.type
+                }],
+                status: config.status.presence
+            };
+            client.user.setPresence(status)
+        }, 5000);
+    },
 };
