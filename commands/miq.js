45005c0e3d2ccba21db578f93c5d0cd054cec1cd
emukun-unagi <153958876+emukun-unagi@users.noreply.github.com>
2024-05-19 14:11:52 +0900

    Create miq.js

---
+++ commands/miq.js
@@ -0,0 +1,35 @@
+module.exports = {
+    name: 'miq',
+    description: 'miq command',
+    async execute(message) {
+        const baseMessage = message.reference;
+        const user = baseMessage.author;
+      
+        const avatarURL = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=1024`;
+
+        const displayName = user.displayName;
+        const name = user.username;
+        const text = content;
+        const icon = user.displayAvatarURL();
+        const brand = "Make it a Quote#6666";
+
+        fetch("https://api.voids.top/fakequote", {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            text: text,
+            avatar: icon,
+            username: name,
+            display_name: displayName,
+            color: "true",
+            watermark: brand,
+          })
+        })
+        .then(response => response.json())
+        .then(data => {
+          message.channel.send(data.url);
+        })
+    },
+};
